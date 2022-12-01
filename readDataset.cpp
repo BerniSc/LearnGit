ab949e287889b81075852bab8999bfdddb0c0c1c
BerniSc <117440705+BerniSc@users.noreply.github.com>
2022-12-01 14:52:13 +0100

    Created ReadDataset.cpp File

---
+++ readDataset.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static string data;
+
+vector<vector<string>> lines;
+
+void printLines(const vector<vector<string>> &vec) {
+    for(int i = 0; i < vec.size(); i++) {
+        for(int j = 0; j < vec[i].size(); j++) {
+            cout << vec[i][j] << endl;
+        }
+        cout << endl;
+    }    
+}
+
+int main() {
+    ifstream myfile;
+    
+    myfile.open("data.txt");
+    
+    vector<string> paragraph;
+    
+    if(myfile.is_open()) {
+        while(myfile) {
+            if(myfile.eof()) data = "";
+            getline(myfile, data);
+            if(data != "") {
+                paragraph.push_back(data);
+            } else {
+                lines.push_back(paragraph);
+                paragraph.clear();
+            }
+        }
+    }
+    printLines(lines);
+
+    return 0;
+}
